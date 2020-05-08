a45f74790e2e082501847e9751362e5d77ffeb74
Karol Będkowski <karol.bedkowski@gmail.com>
2020-05-08 13:01:00 +0200

    firefox_keywords: alternative method to find default profile.
    Sometime default profile is not profile with Default=1. Section
    "Install*" in option Default contain directory name with real default
    profile.
    
    firefox_keywords: adjust get_firefox_home_file; look first on Install
    section; then on Profiles. Also check is path exist - if not, skip it.

--- kupfer/plugin/firefox_keywords.py
+++ kupfer/plugin/firefox_keywords.py
@@ -19,7 +19,7 @@ from kupfer.objects import TextLeaf, TextSource
 from kupfer.obj.helplib import FilesystemWatchMixin
 from kupfer.obj.objects import OpenUrl, RunnableLeaf
 from kupfer import utils
-from kupfer import config, plugin_support
+from kupfer import pretty
 
 __kupfer_settings__ = plugin_support.PluginSettings(
     {
@@ -35,24 +35,56 @@ def get_firefox_home_file(needed_file):
     if not os.path.exists(firefox_dir):
         return None
 
+    def make_absolute_and_check(path):
+        """Helper, make path absolute and check is exist."""
+        if not path.startswith("/"):
+            path = os.path.join(firefox_dir, path)
+
+        if os.path.isdir(path):
+            return path
+
+        return None
+
+
     config = RawConfigParser({"Default" : 0})
     config.read(os.path.join(firefox_dir, "profiles.ini"))
     path = None
 
+    # find Instal.* section and default profile
     for section in config.sections():
-        if config.has_option(section, "Default") and config.get(section, "Default") == "1":
-            path = config.get (section, "Path")
+        if section.startswith("Install"):
+            if not config.has_option(section, "Default"):
+                continue
+
+            # found default profile
+            path = make_absolute_and_check(config.get(section, "Default"))
+            if path:
+                pretty.print_debug(__name__, "found install default profile",
+                                   path)
+                return os.path.join(path, needed_file)
+
             break
-        elif path == None and config.has_option(section, "Path"):
-            path = config.get (section, "Path")
 
-    if path == None:
-        return ""
+    pretty.print_debug("Install* default profile not found")
 
-    if path.startswith("/"):
-        return os.path.join(path, needed_file)
+    # not found default profile, iterate profiles, try to find default
+    for section in config.sections():
+        if not section.startswith("Profile"):
+            continue
 
-    return os.path.join(firefox_dir, path, needed_file)
+        if config.has_option(section, "Default") and \
+                config.get(section, "Default") == "1":
+            path = make_absolute_and_check(config.get(section, "Path"))
+            if path:
+                pretty.print_debug(__name__, "Found profile with default=1",
+                                   section, path)
+                break
+
+        if not path and config.has_option(section, "Path"):
+            path = make_absolute_and_check(config.get(section, "Path"))
+
+    pretty.print_debug(__name__, "Profile path", path)
+    return os.path.join(path, needed_file) if path else ""
 
 def _url_domain(text):
     components = list(urlparse(text))
