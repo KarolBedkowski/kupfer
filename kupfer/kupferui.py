231c5bfcf390156b68f16aef730a879db05322ee
Karol Będkowski <karol.bedkowski@gmail.com>
2023-02-05 10:50:32 +0100

    refactor: kupferui.py; about dialog

--- kupfer/kupferui.py
+++ kupfer/kupferui.py
@@ -1,19 +1,22 @@
 """
 Access functions of Kupfer's Interface
 """
+from __future__ import annotations
+
 import typing as ty
 
 from gi.repository import Gtk
 
 from kupfer import utils, version
 from kupfer.ui.uievents import GUIEnvironmentContext
+from kupfer.ui import preferences
 
 
-def _get_time(ctxenv: ty.Optional[GUIEnvironmentContext]) -> int:
+def _get_time(ctxenv: GUIEnvironmentContext | None) -> int:
     return ctxenv.get_timestamp() if ctxenv else Gtk.get_current_event_time()  # type: ignore
 
 
-def show_help(ctxenv: ty.Optional[GUIEnvironmentContext] = None) -> None:
+def show_help(ctxenv: GUIEnvironmentContext | None = None) -> None:
     """
     Show Kupfer help pages, if possible
     """
@@ -21,7 +24,50 @@ def show_help(ctxenv: ty.Optional[GUIEnvironmentContext] = None) -> None:
         utils.show_url(version.HELP_WEBSITE)
 
 
-_ABOUT_DIALOG = None
+class _AboutDialog:
+    _dialog = None
+
+    @classmethod
+    def get(cls):
+        if cls._dialog is None:
+            cls._dialog = cls._create()
+
+        return cls._dialog
+
+    @classmethod
+    def _create(cls):
+        abdlg = Gtk.AboutDialog()
+        abdlg.set_program_name(version.PROGRAM_NAME)
+        abdlg.set_icon_name(version.ICON_NAME)
+        abdlg.set_logo_icon_name(version.ICON_NAME)
+        abdlg.set_version(version.VERSION)
+        abdlg.set_comments(version.SHORT_DESCRIPTION)
+        abdlg.set_copyright(version.COPYRIGHT)
+        abdlg.set_website(version.WEBSITE)
+        abdlg.set_license(version.LICENSE)
+        abdlg.set_authors(version.AUTHORS)
+        if version.DOCUMENTERS:
+            abdlg.set_documenters(version.DOCUMENTERS)
+
+        if version.TRANSLATOR_CREDITS:
+            abdlg.set_translator_credits(version.TRANSLATOR_CREDITS)
+
+        if version.ARTISTS:
+            abdlg.set_artists(version.ARTISTS)
+
+        abdlg.connect("response", cls._response_callback)
+        abdlg.connect("delete-event", cls._close_callback)
+
+        return abdlg
+
+    @staticmethod
+    def _response_callback(dialog: Gtk.Dialog, response_id: ty.Any) -> None:
+        dialog.hide()
+
+    @classmethod
+    def _close_callback(cls, *_args):
+        cls._dialog = None
+        return True
 
 
 def show_about_dialog(
@@ -30,41 +76,11 @@ def show_about_dialog(
     """
     create an about dialog and show it
     """
-    # Use only one instance, stored in _ABOUT_DIALOG
-    global _ABOUT_DIALOG
-    if _ABOUT_DIALOG:
-        ab = _ABOUT_DIALOG
-    else:
-        ab = Gtk.AboutDialog()
-        ab.set_program_name(version.PROGRAM_NAME)
-        ab.set_icon_name(version.ICON_NAME)
-        ab.set_logo_icon_name(version.ICON_NAME)
-        ab.set_version(version.VERSION)
-        ab.set_comments(version.SHORT_DESCRIPTION)
-        ab.set_copyright(version.COPYRIGHT)
-        ab.set_website(version.WEBSITE)
-        ab.set_license(version.LICENSE)
-        ab.set_authors(version.AUTHORS)
-        if version.DOCUMENTERS:
-            ab.set_documenters(version.DOCUMENTERS)
-        if version.TRANSLATOR_CREDITS:
-            ab.set_translator_credits(version.TRANSLATOR_CREDITS)
-        if version.ARTISTS:
-            ab.set_artists(version.ARTISTS)
-
-        ab.connect("response", _response_callback)
-        # do not delete window on close
-        ab.connect("delete-event", lambda *ign: True)
-        _ABOUT_DIALOG = ab
-
+    dlg = _AboutDialog.get()
     if ctxenv:
-        ctxenv.present_window(ab)
+        ctxenv.present_window(dlg)
     else:
-        ab.present()
-
-
-def _response_callback(dialog: Gtk.Dialog, response_id: ty.Any) -> None:
-    dialog.hide()
+        dlg.present()
 
 
 def show_preferences(ctxenv: GUIEnvironmentContext) -> None:
@@ -80,7 +96,5 @@ def show_preferences(ctxenv: GUIEnvironmentContext) -> None:
 def show_plugin_info(
     plugin_id: str, ctxenv: ty.Optional[GUIEnvironmentContext] = None
 ) -> None:
-    from kupfer.ui import preferences
-
     prefs = preferences.GetPreferencesWindowController()
     prefs.show_focus_plugin(plugin_id, _get_time(ctxenv))
