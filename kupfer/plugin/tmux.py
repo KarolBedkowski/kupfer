fe22cb4f315eebf417aae48a145990775b7c63a9
Karol Będkowski <karol.bedkowski@gmail.com>
2023-03-06 20:56:00 +0100

    feat(plugins): tmux plugin support also tmuxp sessions

--- kupfer/plugin/tmux.py
+++ kupfer/plugin/tmux.py
@@ -1,8 +1,13 @@
 from __future__ import annotations
 
-__kupfer_name__ = _("tmux")
-__kupfer_sources__ = ("TmuxSessionsSource",)
-__description__ = _("Manage tmux sessions")
+"""
+This plugin for simplicity use cli interface instead of api, so we don't need
+additional modules.
+"""
+
+__kupfer_name__ = _("tmux / tmuxp")
+__kupfer_sources__ = ("TmuxSessionsSource", "TmuxpSessionsSource")
+__description__ = _("Manage tmux and tmuxp sessions")
 __version__ = "2023.1"
 __author__ = "Karol Będkowski"
 
@@ -20,7 +25,7 @@ _STATUS = {
 
 
 class TmuxSession(Leaf):
-    """Represented object is the session session_id as string"""
+    """Represented object is the session_id as string"""
 
     def __init__(
         self, sid: str, name: str, windows: str, attached: str, created: str
@@ -92,3 +97,52 @@ class Attach(Action):
         sid = leaf.object
         action_argv = ["tmux", "attach-session", "-t", sid, "-d"]
         utils.spawn_in_terminal(action_argv)
+
+
+class TmuxpSession(Leaf):
+    def __init__(self, name: str) -> None:
+        super().__init__(name, name.capitalize())
+
+    def get_actions(self):
+        yield StartTmuxpSession()
+
+    def get_description(self) -> str:
+        return _("tmuxp saved session")
+
+    def get_icon_name(self):
+        return "gnome-window-manager"
+
+
+class StartTmuxpSession(Action):
+    def __init__(self):
+        name = _("Start session")
+        super().__init__(name)
+
+    def activate(self, leaf, iobj=None, ctx=None):
+        action_argv = ["tmuxp", "load", leaf.object]
+        utils.spawn_in_terminal(action_argv)
+
+
+class TmuxpSessionsSource(Source):
+    """Source for tmuxp sessions"""
+
+    source_use_cache = True
+
+    def __init__(self):
+        super().__init__(_("tmuxp Sessions"))
+
+    def get_items(self):
+        pipe = os.popen("tmuxp ls")
+        output = pipe.read()
+        for line in output.splitlines():
+            if line := line.strip():
+                yield TmuxpSession(line)
+
+    def get_description(self):
+        return _("Active tmux sessions")
+
+    def get_icon_name(self):
+        return "terminal"
+
+    def provides(self):
+        yield TmuxSession
